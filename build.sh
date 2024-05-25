76d3adc840f0e88384b9f7850dee742b0665455e
Balisa16 <sanderantoniusbalisa16@gmail.com>
2024-05-25 10:51:07 +0700

    Remove conan and add thirdparty libraries

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo pip3 install conan
+conan profile detect
+sudo apt install libxv-dev libxxf86vm-dev libxcb-glx0-dev libxcb-render-util0-dev libxcb-xkb-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-randr0-dev libxcb-shape0-dev libxcb-sync-dev libxcb-xfixes0-dev libxcb-xinerama0-dev libxcb-dri3-dev libxcb-cursor-dev libxcb-dri2-0-dev libxcb-dri3-dev libxcb-present-dev libxcb-composite0-dev libxcb-ewmh-dev libxcb-res0-dev
+sudo apt install libxcb-util-dev libxcb-util0-dev
